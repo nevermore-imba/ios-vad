cb8d15371d1d2d1de9032c4f684a5f9ab066c761
baocq <baocq@fenbi.com>
2024-11-08 09:49:07 +0800

    chore: 初始化项目

---
+++ ios-vad/ios_vadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_vadApp.swift
+//  ios-vad
+//
+//  Created by baochuquan on 2024/11/7.
+//
+
+import SwiftUI
+
+@main
+struct ios_vadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
